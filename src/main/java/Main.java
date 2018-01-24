d172950a35d02ab49c3f9af34ac286f8ff176ea2
pintergreg <pintergreg@gmail.com>
2018-01-24 16:16:35 +0100

    First try

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(String.valueOf(add(17, 25)));
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+}
