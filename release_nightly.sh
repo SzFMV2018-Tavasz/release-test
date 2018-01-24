a48c816b8d7fdf6b9e6c01a0104ba0cc3e38ac5c
pintergreg <pintergreg@gmail.com>
2018-01-24 17:42:46 +0100

    release with custom clone

--- release_nightly.sh
+++ release_nightly.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
   echo -e "releasing nightly...\n"
-  mvn clean compile assembly:single
-  cp target/release-test-jar-with-dependencies.jar target/release-test.jar
 
+  cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/SzFMV2018-Tavasz/release-test master > /dev/null
+  cd master
+  mvn clean compile assembly:single
+  cp target/release-test-jar-with-dependencies.jar target/release-test.jar
   tag="$(date +'%Y-%m-%d')-$(git log --format=%h -1)"
 
   git tag $tag
