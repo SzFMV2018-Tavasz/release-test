cc1416b58e84c3c4ee5461955be175f0a214948a
pintergreg <pintergreg@gmail.com>
2018-01-24 17:46:31 +0100

    With fq, without assembly build

--- release_nightly.sh
+++ release_nightly.sh
@@ -7,13 +7,13 @@
   git config --global user.name "travis-ci"
   git clone --quiet --branch=master https://${GH_TOKEN}@github.com/SzFMV2018-Tavasz/release-test master > /dev/null
   cd master
-  mvn clean compile assembly:single
-  cp target/release-test-jar-with-dependencies.jar target/release-test.jar
+#  mvn clean compile assembly:single
+#  cp target/release-test-jar-with-dependencies.jar target/release-test.jar
   tag="$(date +'%Y-%m-%d')-$(git log --format=%h -1)"
 
   git tag $tag
 
-  git push origin $tag
+  git push -fq origin $tag
 
   echo -e "release added\n"
 #fi
