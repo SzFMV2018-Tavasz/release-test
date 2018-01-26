62d1ba9dcb2568dc37f2518107ccd498596710a4
pintergreg <pintergreg@gmail.com>
2018-01-26 13:38:49 +0100

    nightly release with ruby script via gh api

---
+++ release_nightly.rb
@@ -0,0 +1,36 @@
+require 'rest-client'
+require 'json'
+
+$owner = "SzFMV2018-Tavasz"
+$repo = "release-test"
+$tag = "nightly"
+$filename = "./target/release-test.jar"
+$filename_label="release-test.jar"
+$GH_TOKEN = ARGV[0]
+$commit = %x(git log --format=%h -1)
+$release_msg = "Nightly build based on #{$commit} at #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
+
+def create_release
+    response = JSON.parse(RestClient.post "https://api.github.com/repos/#{$owner}/#{$repo}/releases?access_token=#{$GH_TOKEN}", {tag_name: $tag, target_commitish: "master", name: "nightly", body: $release_msg, draft: false, prerelease: true}.to_json)
+    
+    RestClient::Request.execute(method: :post, url: response["upload_url"][0..-14] + "?name='#{$filename_label}'&label=Single runnable jar&access_token=#{$GH_TOKEN}", payload: File.new($filename, "rb"), headers: {'Content-Type': 'application/zip'})
+end
+
+begin
+    get = JSON.parse(RestClient.get "https://api.github.com/repos/#{$owner}/#{$repo}/releases/tags/#{$tag}")
+    
+    # create new build only if the last commit differs
+    if get["body"].include? $commit then
+        puts "No changes from the last nightly build"
+    else
+        RestClient::Request.execute(method: :delete, url: "https://api.github.com/repos/#{$owner}/#{$repo}/releases/#{get["id"]}?access_token=#{$GH_TOKEN}")
+        create_release
+    end
+rescue RestClient::ExceptionWithResponse => err
+    case err.http_code
+    when 404 # if there was no release with the given name
+        create_release
+    else
+        raise
+    end
+end
--- release_nightly.sh
+++ release_nightly.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-#if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+  mvn clean compile assembly:single
+  cp target/release-test-jar-with-dependencies.jar target/release-test.jar
   echo -e "releasing nightly...\n"
-
-#  cd $HOME
-#  git config --global user.email "travis@travis-ci.org"
-#  git config --global user.name "travis-ci"
-#  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/SzFMV2018-Tavasz/release-test master > /dev/null
-#  cd master
-#  mvn clean compile assembly:single
-#  cp target/release-test-jar-with-dependencies.jar target/release-test.jar
-  tag="$(date +'%Y-%m-%d')-$(git log --format=%h -1)"
-
-#  git tag $tag
-
-#  git push -fq origin $tag
-  curl --data '{"tag_name": "$tag","target_commitish": "master","name": "nightly","body": "Nightly build","draft": false,"prerelease": true}' https://api.github.com/repos/SzFMV2018-Tavasz/release-test/releases?access_token=${GH_TOKEN}
-
+  ruby release_nightly.rb $GH_TOKEN
   echo -e "release added\n"
-#fi
+fi
