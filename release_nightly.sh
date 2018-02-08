1147999d99a166128233e34d453ff417d9c66536
pintergreg <pintergreg@gmail.com>
2018-02-08 14:33:40 +0100

    Add user name and email

--- release_nightly.sh
+++ release_nightly.sh
@@ -6,12 +6,17 @@
   cd $HOME
   # git config --global user.email "travis@travis-ci.org"
   # git config --global user.name "travis-ci"
-  git clone --quiet --branch=master "https://jartenmaa:$GH_TOKEN@github.com/SzFMV2018-Tavasz/release-test" master > /dev/null
+  if  [ "$GH_TOKEN" == "" ]; then
+    echo "no token"
+  fi
+  git clone --quiet --branch=master "https://jartenmaa:${GH_TOKEN}@github.com/SzFMV2018-Tavasz/release-test" master > /dev/null
 
   cd master
-  git remote set-url origin "https://jartenmaa:$GH_TOKEN@github.com/SzFMV2018-Tavasz/release-test.git" > /dev/null
+  git config user.email "31477235+jartenmaa@users.noreply.github.com"
+  git config user.name "jartenmaa"
+  git remote set-url origin "https://jartenmaa:${GH_TOKEN}@github.com/SzFMV2018-Tavasz/release-test.git" > /dev/null
   git tag -d nightly
-  git push origin :refs/tags/nightly
+  git push -q origin :refs/tags/nightly
   
   echo -e "releasing nightly...\n"
   ruby release_nightly.rb $GH_TOKEN
